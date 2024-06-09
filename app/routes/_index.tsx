936f1d566b06a356962ce1f018ee65115d26c0d2
andres <andres99.dev@gmail.com>
2024-06-09 15:00:28 +0200

    fix revalidate on params change

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,11 @@ import { json, MetaFunction } from '@remix-run/node'
 import { vacanciesService } from '~/services/vacancies/vacancies.service'
 import { useLoaderData } from '@remix-run/react'
 import { VacanciesChart } from '~/components/vacancies-chart'
+import type { ShouldRevalidateFunction } from '@remix-run/react'
 
+export const shouldRevalidate: ShouldRevalidateFunction = ({ nextParams }) => {
+  return !nextParams
+}
 export const meta: MetaFunction = () => {
   return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }]
 }
